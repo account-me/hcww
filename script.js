4724ca3e142d9befab10fc19b54f9f1f1bc56eff
Bassem <41355488+account-me@users.noreply.github.com>
2022-08-31 05:00:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -64,11 +64,11 @@ $(function() {
     //=========
     $(".btnSendar").click(function() {
 
-        $(location).attr('href','https://egypt-api.netlify.app/');
+        $(location).attr('href','https://egypt-x12.netlify.app/');
     });
     $(".arclick").click(function() {
 
-        $(location).attr('href','https://egypt-api.netlify.app/');
+        $(location).attr('href','https://egypt-x12.netlify.app/');
     });
 
     $("#sendWhats").click(function() {
